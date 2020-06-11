187407a7c3f556f96e73c5c695c6645510feee68
Jeffery Cheng <jeffcheng1234@gmail.com>
2020-06-11 11:19:47 +0800

    .gitignore working

--- test.js
+++
@@ -1,9 +0,0 @@
-const simpleGit = require('simple-git');
-const git = simpleGit(workingDirPath);
-
-const simpleGit = require('simple-git');
-const git = simpleGit(); // or git = simpleGit(workingDir);
-git.init(onInit).addRemote('origin', 'git@github.com:steveukx/git-js.git', onRemoteAdd);
- 
-function onInit (err, initResult) { }
-function onRemoteAdd (err, addRemoteResult) { }
